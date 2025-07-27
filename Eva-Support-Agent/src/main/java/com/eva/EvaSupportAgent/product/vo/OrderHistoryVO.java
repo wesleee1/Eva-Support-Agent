4b5992631019d4fda4f41aafba07e5abab158218
wesleee1 <107876296+wesleee1@users.noreply.github.com>
2025-07-27 22:38:48 +0530

    init commit

---
+++ Eva-Support-Agent/src/main/java/com/eva/EvaSupportAgent/product/vo/OrderHistoryVO.java
@@ -0,0 +1,30 @@
+package com.eva.EvaSupportAgent.product.vo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderHistoryVO {
+	@Schema(hidden = true)
+    private Long id;
+    private Long userId;
+    private String userName;
+    private String userEmail;
+    private String userPhone;
+    private Long productId;
+    private int quantity;
+    private BigDecimal price;
+    private String status;
+    private String productName;
+    private Date orderedOn;
+
+    // Getters and Setters
+}
+
